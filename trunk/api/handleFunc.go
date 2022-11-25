17abd2ef79ffe5ebae3a4ed7ffd4b82f1521f60c
nitingwoshuoa <arlu522691@163.com>
2022-11-25 13:47:00 +0800

    将项目使用包管理

--- trunk/api/handleFunc.go
+++ trunk/api/handleFunc.go
@@ -12,21 +12,21 @@ package api
 
 import (
 	"fmt"
-	"gopkg.in/mgo.v2/bson"
-	"github.com/nitingwoshuoa/shv-zinx_tcp/zdbTool"
-	"github.com/nitingwoshuoa/shv-zinx_tcp/ziface"
 
+	"github.com/nitingwoshuoa/shv-zinx_tcp/ziface"
 	_ "github.com/nitingwoshuoa/shv-zinx_tcp/znet"
 )
 
-func (handle *C2S_Login) Handle(request ziface.IRequest) {
+func (this *C2S_Login) Handle(request ziface.IRequest) {
 	//test
 	fmt.Println("user login succ")
-	fmt.Println(zdbTool.Count("config", "System", bson.M{"_id": 1}))
+
 	//todo 集成protobuf 序列化与反序列化  完成登录逻辑
 }
 
 func (handle *C2S_HeartPackage) Handle(request ziface.IRequest) {
-	request.GetConnection().SetProperty("HeartCount", 0),
-	fmt.Printf("conn == : %d ", request.GetConnection())
+	fmt.Println("HeartPackage succ")
+	request.GetConnection().SetProperty("HeartCount", 0)
+	fmt.Println("HeartPackage succ", request.GetConnection())
+
 }
